e7662890e3faeb6f23f27582c57c6e5ff77ef578
emorales8 <73521027+emorales8@users.noreply.github.com>
2020-10-30 04:35:39 -0700

    Update post.go

--- post.go
+++ post.go
@@ -28,7 +28,7 @@ var err error
 func connect() {
 	err = godotenv.Load()
 	if err != nil {
-		log.Fatalf("Unable to load env file %v", err)
+		log.Fatalf("Unable to load env file! %v", err)
 	}
 	fmt.Println(os.Getenv("DB_HOST"))
 	DBURL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
